256404028cc90e709f5a64b1683fea381fa8cef4
Maria Z <z0559882272@gmail.com>
2025-02-09 19:14:17 +0200

    added Login success

--- src/main/java/manager/HelperBase.java
+++ src/main/java/manager/HelperBase.java
@@ -1,6 +1,8 @@
 package manager;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 
 public class HelperBase {
 
@@ -9,4 +11,26 @@ public class HelperBase {
     public HelperBase(WebDriver wd) {
         this.wd = wd;
     }
+
+    public void click(By locator) {
+        wd.findElement(locator).click();
+    }
+
+    public void type(By locator, String text){
+        WebElement element = wd.findElement(locator);
+        element.click();
+        element.clear();
+        if (text !=null){
+            element.sendKeys(text);
+        }
+    }
+
+    public void pause(int time){
+        try {
+            Thread.sleep(time);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
 }
--- src/test/java/tests/LoginTests.java
+++ src/test/java/tests/LoginTests.java
@@ -1,11 +1,19 @@
 package tests;
 
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class LoginTests extends TestBase {
 
     @Test
-    public void test(){
+    public void loginSuccess() {
+        app.getHelperUser().openLoginForm();
+        app.getHelperUser().fillLoginForm("marga@gmail.com", "Mmar123456$");
+        app.getHelperUser().submitLogin();
 
+//Assert if element with text "Logged in success" is present
+        Assert.assertEquals(app.getHelperUser().getMessage(),"Logged in success");
+
+        app.getHelperUser().clickOKButton();
     }
 }
